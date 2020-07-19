5e1d8fddc8cd189e22efea2222ef2a1d680e4999
4erniikot <4erniikot@gmail.com>
2020-07-19 12:35:35 +0200

    Added screenshot, minor fixes

--- symulowanie_wyżarzania_komiwojażera/Program.cs
+++ symulowanie_wyżarzania_komiwojażera/Program.cs
@@ -11,20 +11,26 @@ namespace symulowanie_wyżarzania_komiwojażera
     /// 2. Losowa zamiana 2 miast w kolejce
     /// 3. Porownanie czy calkowity dystans nowej trasy jest lepszy od starego lub spelnia ona warunek Boltzmana
     /// 4. Jesli 3 punkt jest prawda to zastapiamy stara kolejku na nowa
-    /// 5. Jesli temperatura > epsilon idz do kroku 2
+    /// 5. Zmniejszenie temperatury
+    /// 6. Jesli temperatura > epsilon idz do kroku 2, w przeciwnym przypadku idz do kroku 7
+    /// 7. Koniec programu
     /// </summary>
-    public class TravellingSalesmantravellingsalesmanproblem
+    public class TravellingSalesmanProblemSimulatedAnnealing
     {
-        public List<int> currentOrder = new List<int>();
+        private List<int> currentOrder = new List<int>();
         private List<int> nextOrder = new List<int>();
         private double[,] distances;
         private Random random = new Random();
-        public double shortestDistance = 0;
+        private double bestDistance = 0;
+        private string cities;
 
-      
-        private void LoadCities()   //inicjalizacja miast z pliku
+        public TravellingSalesmanProblemSimulatedAnnealing(string cities)
         {
-            string cities = File.ReadAllText("Miasta.txt");
+            this.cities = cities;
+        }
+
+        private void InitCities()   //inicjalizacja miast z pliku
+        {
             string[] rows = cities.Split('\n');
             distances = new double[rows.Length, rows.Length];
 
@@ -32,15 +38,13 @@ namespace symulowanie_wyżarzania_komiwojażera
             {
                 string[] distance = rows[i].Split(' ');
                 for (int j = 0; j < distance.Length; j++)
-                {
                      distances[i, j] = double.Parse(distance[j]);   //PILNE: uwazac na znaki w pliku oraz puste linii i ewentualnie zamenic "," na "." (moze spowodowac blad)
-                }
 
                 currentOrder.Add(i);    //ilosci miast odpowiada ilosc wierszow w naszym plikie, dodajemy ich po kolei do kolejki
             }
         }
 
-        private double CountDistance(List<int> order)   //liczymy wage (odleglosc) trasy wedlug przekazanej kolejki
+        private double RouteDistance(List<int> order)   //liczymy wage (odleglosc) trasy wedlug przekazanej kolejki
         {
             double distance = 0;
 
@@ -53,70 +57,87 @@ namespace symulowanie_wyżarzania_komiwojażera
             return distance;
         }
 
-        private List<int> Swap2Cities(List<int> order)   //bierzemy 2 losowych miasta i zamieniamy ich miejscami w kolejce
+        private List<int> SwapCities(List<int> order)   //bierzemy 2 losowych miasta i zamieniamy ich miejscami w kolejce
         {
             List<int> newOrder = new List<int>();
+            int firstCity;
+            int secondCity;
+            int tmp;
 
-            for (int i = 0; i < order.Count; i++)
-                newOrder.Add(order[i]);
+            foreach (var city in order)
+                newOrder.Add(city);
 
-            int firstRandomCityIndex = random.Next(1, newOrder.Count);  //zaczynamy od 0, wiec losujemy w zakresie od 1 miasta do miasta N (ostatniego indeksu)
-            int secondRandomCityIndex = random.Next(1, newOrder.Count);
+            firstCity = random.Next(1, newOrder.Count);  //zaczynamy od 0, wiec losujemy w zakresie od 1 miasta do miasta N (ostatniego indeksu)
+            secondCity = random.Next(1, newOrder.Count);
 
-            int tmp = newOrder[firstRandomCityIndex];
-            newOrder[firstRandomCityIndex] = newOrder[secondRandomCityIndex];
-            newOrder[secondRandomCityIndex] = tmp;
+            tmp = newOrder[firstCity];
+            newOrder[firstCity] = newOrder[secondCity];
+            newOrder[secondCity] = tmp;
 
             return newOrder;
         }
 
-        public void Anneal()    //wyzarzanie 
+        public void AnnealingProcess()    //wyzarzanie 
         {
-            double temperature = 10000.0;
-            double deltaDistance = 0;
-            double cooling = 0.9999;
-            double eps = 0.00001;
+            double coolingRate = 0.99999;
+            double Epsilon = 0.000000001;
+            double delta;
+            double tmpDistance;
 
-            LoadCities();
+            InitCities();
+            tmpDistance = RouteDistance(currentOrder);  //liczymy dystans trasy w przypadku bez zadnych zmian
 
-            double distance = CountDistance(currentOrder);  //liczymy dystans trasy w przypadku bez zadnych zmian
-
-            while (temperature > eps)
+            for (double temperature = 100000.0; temperature > Epsilon; temperature *= coolingRate)
             {
-                nextOrder = Swap2Cities(currentOrder);   //zamieniamy 2 miasta
-
-                deltaDistance = CountDistance(nextOrder) - distance;    //porownujemy nowy dystans trasy (po zamianie tych 2 miast miejscami) ze starym
+                nextOrder = SwapCities(currentOrder);   //zamieniamy 2 miasta
+                delta = RouteDistance(nextOrder) - tmpDistance;    //porownujemy nowy dystans trasy (po zamianie tych 2 miast miejscami) ze starym
                 
-                if ((deltaDistance < 0) || (distance > 0 && Math.Exp(-deltaDistance / temperature) > random.NextDouble())) //zastapiamy stara trase na nowa w przypadku gdy nowa trasa ma mniejszy dystans albo spelnia ona warunek Boltzmana
+                if ((delta < 0) || (tmpDistance > 0 && Math.Exp(-delta / temperature) > random.NextDouble())) //zastapiamy stara trase na nowa w przypadku gdy nowa trasa ma mniejszy dystans albo spelnia ona warunek Boltzmana
                 {
                     for (int i = 0; i < nextOrder.Count; i++)
                         currentOrder[i] = nextOrder[i];
 
-                    distance = deltaDistance + distance;
-                }
-              
-                temperature *= cooling; //zmniejszamy temperature
+                    tmpDistance = delta + tmpDistance;
+                }              
             }
 
-            shortestDistance = distance;
+            bestDistance = tmpDistance;
         }
+
+        public string OutputScheme()
+        {
+            string result = "";
+
+            for (int i = 0; i < currentOrder.Count; i++)
+                if (!i.Equals(currentOrder.Count-1))
+                    result += currentOrder[i] + " -> ";
+                else
+                    result += currentOrder[i];
+            
+            return result;
+        }
+
+        public override string ToString()               //wypisujemy wynik
+        {
+            string route = "Shortest route (city indexes): " + OutputScheme();
+            string totalCities = "Total cities: " + currentOrder.Count.ToString();
+            string shortestDistance = "The shortest distance is: " + this.bestDistance.ToString();
+
+            string outputString = string.Format("{0}{1}{2}{3}{4}", route, Environment.NewLine, totalCities, Environment.NewLine, shortestDistance);
+            return outputString;
+        }
+
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            TravellingSalesmantravellingsalesmanproblem travellingsalesmanproblem = new TravellingSalesmantravellingsalesmanproblem();
-            travellingsalesmanproblem.Anneal();
+            string cities = File.ReadAllText("Miasta.txt");
+            TravellingSalesmanProblemSimulatedAnnealing travellingSalesmanProblemSimulatedAnnealing = new TravellingSalesmanProblemSimulatedAnnealing(cities);
+            travellingSalesmanProblemSimulatedAnnealing.AnnealingProcess();
 
-            string result = "";
-
-            foreach (var tmp in travellingsalesmanproblem.currentOrder)
-                result += tmp + " ";
-
-            Console.WriteLine("Shortest Route: " + result);
-            Console.WriteLine("Total cities: " + travellingsalesmanproblem.currentOrder.Count.ToString());
-            Console.WriteLine("The shortest distance is: " + travellingsalesmanproblem.shortestDistance.ToString());
+            Console.WriteLine(travellingSalesmanProblemSimulatedAnnealing.ToString());
         }
     }
 }
